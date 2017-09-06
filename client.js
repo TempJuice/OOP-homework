b3a6f7813c47ad4babf4be45a1feb5249763ad1c
Ryan Templin <tempjuice@hotmail.com>
2017-09-06 09:28:35 -0500

    Tried adding a method.

--- client.js
+++ client.js
@@ -1,16 +1,29 @@
 console.log('sourced');
 
 class Monster {
-    constructor(height, weight, fur, scariness) {
-    this.height = height;
-    this.weight = weight;
-    this.fur = fur;
-    this.scariness = scariness;
+    constructor(height, weight, fur) {
+        this.height = height;
+        this.weight = weight;
+        this.fur = fur;
     }
 };
 
-let yeti = new Monster (7, 540, 'white', 77);
+class Yeti extends Monster {
+
+}
+
+let yeti = new Monster(7, 540, 'white');
 console.log(yeti);
+
+howScary(scariness) {
+    if (this.height > 8 && this.weight > 350) {
+        scariness = 'Scary as fuck!';
+    } else {
+        scariness = 'Not that scary...';
+    }
+}// end of howScary method
+
+let locness = new Monster()
 
 
 
