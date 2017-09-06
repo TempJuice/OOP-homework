7ed0228f987b58e3700179f635a8410707ca387e
Ryan Templin <tempjuice@hotmail.com>
2017-09-06 08:14:51 -0500

    New class created.

---
+++ client.js
@@ -0,0 +1,16 @@
+console.log('sourced');
+
+class Monster {
+    constructor(height, weight, fur, scariness) {
+    this.height = height;
+    this.weight = weight;
+    this.fur = fur;
+    this.scariness = scariness;
+    }
+};
+
+let yeti = new Monster (7, 540, 'white', 77);
+console.log(yeti);
+
+
+
